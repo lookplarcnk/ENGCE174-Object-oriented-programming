dd8774178106951734dcd769ce2549f5b5b94bc7
lookplarcnk <83052144+lookplarcnk@users.noreply.github.com>
2024-07-25 14:11:02 +0700

    Update lab4.2.py

--- LAB04/lab4.2.py
+++ LAB04/lab4.2.py
@@ -3,4 +3,4 @@
 # Creating a list of squares of numbers from 1 to 5 สร้างลิสต์ของเลขยกกำลังสองจาก 1 ถึง 5
 squares = [x ** 2 for x in range(1, 6)]
 
-print("Squares:", squares) # Output: [1, 4, 9, 16, 25] # สร้างลิสต์ของเลขยกกำลังสองจาก 1 ถึง 5
+print("Squares:", squares) # Output: [1, 4, 9, 16, 25]
